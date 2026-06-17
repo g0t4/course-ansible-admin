24921bf215df755ebca43940ce0aac89161dc5ec
Wes Higbee <wes.mcclure@gmail.com>
2026-06-17 17:08:25 -0500

    mkdir too

--- lab/host/host-ssh.sh
+++ lab/host/host-ssh.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+mkdir -p ~/.ssh/config.d/vagrants
+
 echo '## find running VMs only'
 set running_vms (vagrant status --machine-readable | grep 'state,running' | cut -d',' -f2 | sort | uniq)
 echo found $running_vms
